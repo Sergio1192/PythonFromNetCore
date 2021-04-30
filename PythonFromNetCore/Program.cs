2e5f7529815e8ab0af7447815538687902ed99fb
Sergio Merino Martinez <smerinomartinez@gesternova.com>
2021-04-30 15:30:41 +0200

    Agregar archivos de proyecto.

---
+++ PythonFromNetCore/Program.cs
@@ -0,0 +1,26 @@
+﻿using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using System;
+
+namespace PythonFromNetCore
+{
+    class Program
+    {
+        private const string PYTHON_FILE_PATH = @"PythonFiles\sample.py";
+
+        static void Main(string[] args)
+        {
+            ScriptRuntime py = Python.CreateRuntime();
+            
+            // Execute Script
+            dynamic pySample = py.UseFile(PYTHON_FILE_PATH);
+            
+            // Execute function in script
+            pySample.Greeting("Sergio");
+
+            // Get result from function in script
+            var name = pySample.GetName();
+            Console.WriteLine($"Hello {name}");
+        }
+    }
+}
---
+++ PythonFromNetCore/PythonFiles/sample.py
@@ -0,0 +1,7 @@
+﻿print("Hello world!")
+
+def Greeting(name):
+    print("Hello " + name)
+
+def GetName():
+    return "World"
